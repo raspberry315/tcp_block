5024c674c3d99350ef460092eaf2c13af497835b
raspberry315 <raspberry315@naver.com>
2017-11-30 17:55:40 +0900

    Update tcp_block.py

--- tcp_block.py
+++ tcp_block.py
@@ -2,6 +2,7 @@ from scapy.all import *
 from threading import Timer
 import os
 
+fakeData = 'HTTP/1.1 302 Found\r\nLocation: http://test.gilgil.net\n\n'
 
 def isHttpRequest(data):
     headers = str(data).splitlines()
@@ -24,17 +25,14 @@ def send_backward_rst(pkt):
 
 
 def send_fakepkt(pkt):
-    fakeData2 = 'HTTP/1.1 302 Found\r\nLocation: http://test.gilgil.net\n\n'
     fake = Ether(dst=pkt[Ether].src, src=pkt[Ether].dst) / IP(dst=pkt[IP].src, src=pkt[IP].dst) / TCP(
         dport=pkt[TCP].sport,
         sport=pkt[TCP].dport,
         flags="FA",
         seq=pkt[TCP].ack,
-        ack=pkt[TCP].seq + (len(pkt[TCP].payload) if pkt.getlayer(Raw) else 1)
-    ) / fakeData2
-
+        ack=pkt[TCP].seq + len(pkt[TCP].payload)
+    ) / fakeData
     sendp(fake)
-    fake.show2()
 
 
 def send_forward_rst(pkt):
@@ -42,11 +40,10 @@ def send_forward_rst(pkt):
         dport=pkt[TCP].dport,
         sport=pkt[TCP].sport,
         flags="RA",
+        seq=pkt[TCP].seq + (len(pkt[TCP].payload) if pkt.getlayer(Raw) else 0),
+        ack=pkt[TCP].ack + (len(fakeData) if pkt.getlayer(Raw) else 0)
     )
-    fake[TCP].seq += (len(pkt[TCP].payload) if pkt.getlayer(Raw) else 1)
-
     sendp(fake)
-    fake.show2()
 
 
 def attack(pkt):
@@ -54,10 +51,10 @@ def attack(pkt):
     if pkt[TCP].flags & 0x04 or pkt[TCP].flags & 0x01: return
 
     if pkt.getlayer(Raw) and isHttpRequest(layer.load):  # http
-        #pkt.show2()
         print "[+]=========HTTP sending rst============"
         send_forward_rst(pkt)
         send_fakepkt(pkt)
+
     else:  # only tcp
         send_forward_rst(pkt)
         send_backward_rst(pkt)
